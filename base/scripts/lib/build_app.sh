4577c51beb17a2738ac66d5d6fb334bfe2d70d7d
Joakim Ramer <jramer@me.com>
2016-10-24 11:04:11 +0200

    run npm install before build

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -8,7 +8,9 @@ BUNDLE_DIR=/tmp/bundle-dir
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
 
-meteor --release 1.3.2 build --directory $BUNDLE_DIR --server=http://localhost:3000
+# If package.json exists, use the meteor npm command to install the production dependencies
+[ -e package.json ] && meteor npm install --production
+meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
 
 cd $BUNDLE_DIR/bundle/programs/server/
 npm i
