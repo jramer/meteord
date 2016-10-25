69afb82d606b77f75538116f2fe7028b0243f1c0
Joakim Ramer <jramer@me.com>
2016-10-25 08:37:55 +0200

    Update build_app.sh

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -9,7 +9,8 @@ cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
 
 # If package.json exists, use the meteor npm command to install the production dependencies
-[ -e package.json ] && meteor --release 1.3.2 npm install --production
+# [ -e package.json ] && 
+meteor --release 1.3.2 npm install --production
 meteor --release 1.3.2 build --directory $BUNDLE_DIR --server=http://localhost:3000
 
 cd $BUNDLE_DIR/bundle/programs/server/
